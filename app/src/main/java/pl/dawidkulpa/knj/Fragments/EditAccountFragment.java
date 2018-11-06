7fc2f6d191b046a173263b87f248e9ce338a74d1
Dawid Kulpa <1dawidk@gmail.com>
2018-11-07 00:23:14 +0100

    Utworzenie layoutów wyświetlania informacji o koncie i edycji konta zgodnie z mokapami v1

---
+++ app/src/main/java/pl/dawidkulpa/knj/Fragments/EditAccountFragment.java
@@ -0,0 +1,51 @@
+package pl.dawidkulpa.knj.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pl.dawidkulpa.knj.R;
+
+public class EditAccountFragment extends Fragment {
+    public EditAccountFragment() {
+        // Required empty public constructor
+    }
+
+    public static EditAccountFragment newInstance() {
+        EditAccountFragment fragment = new EditAccountFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calendar, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        //if (context instanceof OnFragmentInteractionListener) {
+        //   mListener = (OnFragmentInteractionListener) context;
+        //} else {
+        //throw new RuntimeException(context.toString()
+        //          + " must implement OnFragmentInteractionListener");
+        //}
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
